e672a99845cbd929d5b3e007ecd5c6c187eb22a1
Usman Farooq <anovius10@gmail.com>
2022-09-30 16:43:38 +0500

    developer link

--- bin/main.js
+++ bin/main.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 var lib = require('../lib/index.js');
 let args = process.argv.slice(2);
+if (!args[0]) {
+    console.log('Please provide a path to your JSON file.');
+    return;
+}
 lib.main(args[0]);
