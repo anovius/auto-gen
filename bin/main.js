0ed0a2b1e769df399f61a505aaec1745a7ed936d
Usman Farooq <anovius10@gmail.com>
2022-09-30 16:49:11 +0500

    error handlers

--- bin/main.js
+++ bin/main.js
@@ -6,3 +6,16 @@ if (!args[0]) {
     return;
 }
 lib.main(args[0]);
+
+global.errorHandler = (err) => {
+    clearInterval(global.loader);
+    console.log('\x1b[31m%s\x1b[0m', '\bSomething went wrong. Please check your JSON file format.');
+}
+
+process.on('unhandledRejection', (reason, p) => {
+    errorHandler(reason);
+})
+
+process.on('uncaughtException', (err) => {
+    errorHandler(err);
+})
